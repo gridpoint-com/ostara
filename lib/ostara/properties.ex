2e9b7d1b6201c299c76cabf5ffd539030bcfe3bd
Jason <jasonmj@gmail.com>
2023-07-14 15:03:56 -0400

    Support Ecto.Enum

--- lib/ostara/properties.ex
+++ lib/ostara/properties.ex
@@ -34,8 +34,12 @@ defmodule Ostara.Properties do
     source.__schema__(:fields)
   end
 
-  @spec propertize_type(atom()) :: map()
+  @spec propertize_type(atom() | {:parameterized, struct(), map()}) :: map()
   defp propertize_type(:binary_id) do
+    %{"type" => "string"}
+  end
+
+  defp propertize_type({:parameterized, Ecto.Enum, _params}) do
     %{"type" => "string"}
   end
 
